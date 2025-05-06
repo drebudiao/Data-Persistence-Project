55f2573ece50bf6a042e40ef14b0240822d0f3c5
D@nr3b <drebudiaojr@gmail.com>
2025-05-06 23:30:42 +0800

    Added Best Score upon submit
    Modified Start Menu Handler:
    1.) Added playerNameInputSubmit()
    2.) Modified Start() to add as listener to onSubmit for Player Name Input

--- Assets/Scripts/StartMenuHandler.cs
+++ Assets/Scripts/StartMenuHandler.cs
@@ -3,38 +3,57 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
 
 public class StartMenuHandler : MonoBehaviour
 {
     public TMP_InputField playerNameInput;
-    public TextMeshProUGUI errorMessage;
+    public TextMeshProUGUI messageText;
+    public Button startButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Add On Submit listner to Player Name Input
+        playerNameInput.onSubmit.AddListener(playerNameInputSubmit);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void StartButtonClick()
     {
         if (playerNameInput.text == "")
         {
-            errorMessage.text = "Please input a new or existing player name.";
-            errorMessage.gameObject.SetActive(true);
+            DisplayErrorMessage("Please input a new or existing player name.");
         }
         else
         {
-            errorMessage.text = "";
-            errorMessage.gameObject.SetActive(false);
-
             // Load the Scene
             SceneManager.LoadScene(1);
         }
+    }
+
+    private void DisplayErrorMessage(string errorMsg)
+    {
+        messageText.text = errorMsg;
+        messageText.color = Color.red;
+        messageText.gameObject.SetActive(true);
+    }
+    private void DisplayBestScore()
+    {
+        messageText.text = "Best Score: 000";
+        messageText.color = Color.black;
+        messageText.gameObject.SetActive(true);
+    }
+
+    private void HideMessage()
+    {
+        messageText.text = "";
+        messageText.gameObject.SetActive(false);
     }
 
     public void QuitButtonClick()
@@ -45,4 +64,17 @@ public class StartMenuHandler : MonoBehaviour
         Application.Quit(); // original code to quit Unity player
 #endif
     }
+
+    private void playerNameInputSubmit(string text)
+    {
+        if (text == "")
+        {
+            DisplayErrorMessage("Please input a new or existing player name.");
+        }
+        else
+        {
+            DisplayBestScore();
+        }
+    }
+
 }
