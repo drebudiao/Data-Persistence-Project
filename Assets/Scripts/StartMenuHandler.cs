cde70d3358476f4a3e4144072310b35ce4cd2916
D@nr3b <drebudiaojr@gmail.com>
2025-05-06 23:02:15 +0800

    Updated StartMenuHandler Script
    Updates to StartMenuHandler:
    1.) Added StartButtonClick()
    2.) Added QuitButtonClick()
    
    Updates to the Start Menu Scene:
    1.) Bind StartButtonClick() to Start Button
    2.) Bind QuitButtonClick() to Quit Button

--- Assets/Scripts/StartMenuHandler.cs
+++ Assets/Scripts/StartMenuHandler.cs
@@ -1,16 +1,48 @@
+using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenuHandler : MonoBehaviour
 {
+    public TMP_InputField playerNameInput;
+    public TextMeshProUGUI errorMessage;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartButtonClick()
+    {
+        if (playerNameInput.text == "")
+        {
+            errorMessage.text = "Please input a new or existing player name.";
+            errorMessage.gameObject.SetActive(true);
+        }
+        else
+        {
+            errorMessage.text = "";
+            errorMessage.gameObject.SetActive(false);
+
+            // Load the Scene
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void QuitButtonClick()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
 }
